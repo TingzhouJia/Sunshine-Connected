b8c75efd4b102da3cfbbfaf1b060e73b2016690f
TingzhouJia <joe.tzjia@gmail.com>
2020-07-14 17:18:58 -0400

    :rocket: add fluent-ui

---
+++ web_react/i18n.ts
@@ -0,0 +1,23 @@
+import NextI18Next from 'next-i18next'
+import { NextComponentType, NextPageContext } from 'next'
+import { useTranslation as originalUseTranslation } from 'react-i18next'
+export const useTranslation = originalUseTranslation
+export const nextI18next = new NextI18Next({
+  defaultLanguage: 'en',
+  localeSubpaths: {
+    en: 'en',
+    fr: 'fr',
+  },
+  localePath: typeof window === 'undefined' ? 'public/locales' : 'locales',
+  fallbackLng: 'en',
+  otherLanguages: ['fr'],
+})
+
+export const appWithTranslation = nextI18next.appWithTranslation
+export const Trans = nextI18next.Trans
+
+export type I18nPage<P = {}> = NextComponentType<
+  NextPageContext,
+  { namespacesRequired: string[] },
+  P & { namespacesRequired: string[] }
+>
---
+++ web_react/layout/app_layout.tsx
@@ -0,0 +1,3 @@
+export const AppLayout: React.FC = () => {
+  return <div></div>
+}
